6a0afd82726dedec7f2370e82f0f402ab133ef18
Mahesh Gulivindala <gulivindala.mahesh@specsavers.com>
2022-12-14 18:11:38 +0530

    react redux demo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import './App.css'
+import CakeView from '../features/cake/cakeView';
+import IceCreamView from '../features/icecream/icecreamView';
+import UserView from '../features/users/userView';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <CakeView/>
+      <IceCreamView/>
+      <UserView/>
+    </div>
+  )
+}
+
+export default App
